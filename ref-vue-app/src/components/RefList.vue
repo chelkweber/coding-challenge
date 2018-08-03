fc7f5d6b773710e004550f789acefe653e9aaba3
Chelsea Weber <chelseaweber92@gmail.com>
2018-08-02 22:00:42 -0400

    labels

--- ref-vue-app/src/components/RefList.vue
+++ ref-vue-app/src/components/RefList.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="ref-list">
+    <div class="labels">
+      <p class="title">Link Title</p>
+      <p class="clicks">Clicks</p>
+      <p class="edit">Edit</p>
+      <p class="delete">Delete</p>
+    </div>
     <ul>
       <RefLink v-on:deleteLink="deleteLink" v-on:sendToLandingPage="sendToLandingPage" v-for="link in links" :link.sync="link"></RefLink>
     </ul>
@@ -44,6 +50,26 @@
     width: 90%;
     max-width: 500px;
     margin: 0 auto;
+  }
+  
+  .labels {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-around;
+    align-items: center;
+    align-content: center;
+  }
+  
+  .labels .title {
+    display: inline;
+    text-align: left;
+    width: 40%;
+    flex-shrink: 0;
+  }
+  
+  .ref-link p {
+    text-align: center;
   }
   
   .ref-list ul{
